b6d6dde22e97301fae082ab7171ae98ff115b352
himanshu-sagar <sagarhimanshu355@gmail.com>
2022-07-30 21:11:25 +0530

    commit

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import os
+import os, json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,3 +132,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DATA = json.load(open(os.path.join(BASE_DIR, 'data.json')))
