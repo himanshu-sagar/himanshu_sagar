174288b6a94779d25f20c5b93cd6c95372ddcffe
himanshu-sagar <sagarhimanshu355@gmail.com>
2022-07-30 21:11:15 +0530

    commit

--- portfolio/main/views.py
+++ portfolio/main/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.conf import settings
 
 def index(request):
-    context = {}
+    context = {
+        "name": settings.DATA.get("name"),
+        "about_me": settings.DATA.get("about_me"),
+        "skills": settings.DATA.get("skills"),
+    }
     return render(request, 'main/index.html', context)
+
+def projects(request):
+    context = {
+        "projects": settings.DATA.get("projects"),
+    }
+    return render(request, 'main/projects.html', context)
